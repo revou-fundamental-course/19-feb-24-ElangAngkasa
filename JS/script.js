6ecadaeef30bc7a8dfb9c65ae69c1aeec6a18151
Hanif Ahmad <aahmad.hanif74@gmail.com>
2024-02-29 21:37:59 +0700

    first set up

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,20 @@
 //var sama let bisa mengubah id
 //const tidak bisa diganti-> untuk menangani hacker
-
+/*
 function repalceName(){
     let namep = prompt("Namamu siapa bang?","");
     document.getElementById("namep").innerHTML = namep
 }
 repalceName();
 
-let tombol = document.getElementById("tombol")
+let tombol = document.getElementById("tombol");
 tombol.addEventListener("click", function(){
     repalceName();
+})
+*/
+let nameInput = document.getElementById("name-sender");
+
+document.getElementById("button-submit").addEventListener("click",function(){
+    const namep = nameInput.value;
+    document.getElementById("namep").innerHTML= namep;
 })
